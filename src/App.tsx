57df206e3c0d9cfce8e09fe6815d75e2f3839838
omgpiu <dyba@List.ru>
2021-03-10 06:51:55 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import st from './App.module.css';
+import {List} from './main/List';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={st.App}>
+            <List/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/main/List.tsx
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import st from '../App.module.css';
+
+type objType = {
+    name: string
+    surname: string
+    number: number
+}
+
+export const List = () => {
+    const newObjArray = [] as objType[];
+    const [data, setData] = useState(generateArray());
+    const [resultArr, setResultArr] = useState<objType[]>([]);
+    useEffect(() => {
+        plus20(resultArr, data);
+    }, []);
+
+    function plus20(arr1res: objType[], arr2Date100: objType[]) {
+        let tempArr = arr2Date100.slice(arr1res.length, arr1res.length + 20);
+        setResultArr((arr1res) => [...arr1res, ...tempArr]);
+    }
+
+    function generateArray() {
+        const names = ['Fariz', 'Falisha', 'Mami', 'Defina', 'Fiska', 'Papi'];
+        for (let i = 0; i < 100; i++) {
+            newObjArray[i] = {
+                name: names[(Math.floor(Math.random() * (names.length)))],
+                surname: names[(Math.floor(Math.random() * (names.length)))] + Math.random().toString(36).substr(2, 3),
+                number: i
+            };
+        }
+        return newObjArray;
+    }
+
+    const scrollHandler = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+        const element = e.currentTarget;
+
+        if (resultArr.length <= 100) {
+            if (Math.abs((element.scrollHeight - element.scrollTop) - element.clientHeight) < 1) {
+                console.log('scroll');
+                plus20(resultArr, data);
+            }
+        }
+    };
+
+    return <div className={st.wrapper}
+                onScroll={scrollHandler}>
+
+        {resultArr.map((m, index) =>
+
+            <div className={st.magic}>
+                {m.name + `  ${m.surname}`}
+
+            </div>
+        )
+        }
+
+    </div>;
+};
+;
